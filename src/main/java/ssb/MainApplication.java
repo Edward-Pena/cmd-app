4d405d327a1a34f9b9bc11fa929de5fdbcf52bf3
Edward-Pena <penalalo016@gmail.com>
2023-06-13 15:35:36 -0600

    Adding query to select only bed info

--- src/main/java/ssb/MainApplication.java
+++ src/main/java/ssb/MainApplication.java
@@ -6,6 +6,7 @@ import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import ssb.dto.Room;
 
 import javax.sql.DataSource;
@@ -21,15 +22,16 @@ public class MainApplication implements QuarkusApplication {
 
     @Inject
     EntityManager entityManager;
-
+    @ConfigProperty(name = "application.bed-info")
+    private String bedInfo;
 
     @Override
     public int run(String... args) throws Exception {
         Arc.container().requestContext().activate();
-    List<Room> rooms = entityManager.createQuery("Select r from Room r", Room.class).getResultList();
+    List<Room> rooms = entityManager.createQuery("select r from Room r where r.bedInfo=:bedInfo", Room.class)
+            .setParameter("bedInfo",bedInfo).getResultList();
     rooms.forEach(System.out::println);
     Arc.container().requestContext().deactivate();
-        System.out.println("aaaaaaaaaaaaaaaaaaa------------------");
-
+        System.out.println("AAAAAAAAA"+bedInfo);
         return 0;
 }}
